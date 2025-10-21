fcb6a66a5e3e8c42d1b142480dd640477087622f
OneFluffyKiriko <danko.gakman@gmail.com>
2025-10-21 16:28:49 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Variant 7: Дано трьохзначне число. Вивести першу та останню цифру даного числа.
+//Вхідні дані: d = 357 .
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+namespace Labwork1
+{
+    class App
+    {
+        static void Main(string[] args)
+        {   
+            int d = 357;
+            int a = d / 100;
+            int b = d % 10;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+
+}
+
