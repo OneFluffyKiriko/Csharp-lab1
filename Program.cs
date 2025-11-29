4256dd9437b75ae03010a72cf6d1b3d0ff807a2b
OneFluffyKiriko <danko.gakman@gmail.com>
2025-11-30 00:37:14 +0200

    so basically, this was a failiure

--- Program.cs
+++ Program.cs
@@ -1,25 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Variant 7: Дано трьохзначне число. Вивести першу та останню цифру даного числа.
-//Вхідні дані: d = 357 .
+﻿
 using System;
 // using System.Collections.Generic;
 // using System.Linq;
 // using System.Text;
 // using System.Threading.Tasks;
 
-namespace Labwork1
+namespace Labwork1;
+
+class Program
 {
-    class App
+    public static (int num1, int num2) FirstLast(int d)
     {
-        static void Main(string[] args)
-        {   
-            int d = 357;
-            int a = d / 100;
-            int b = d % 10;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-        }
+        int num1 = d / 100;
+        int num2 = d % 10;
+        Console.WriteLine($"First Number = {num1}, Last Number = {num2}");
+        return (num1, num2);
     }
 
+    static void Main(string[] args)
+    {   
+    //7: Дано трьохзначне число. Вивести першу та останню цифру даного числа.
+        //Вхідні дані: d = 357 .
+    FirstLast(357);
+
+        
+    }
 }
+
+
 
